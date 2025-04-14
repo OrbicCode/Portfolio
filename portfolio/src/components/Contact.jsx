83c2db2c10537355a03018133562a2525c04190c
James O'Kane <orbiccode@gmail.com>
2025-04-14 15:47:24 +0100

    grid layout changes

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -4,6 +4,19 @@ export default function Contact() {
     return (
         <section className={`${styles.contact} contact`}>
             <h2>Contact</h2>
+            <h3>Available for hire</h3>
+            <p>
+            Want me to join your team?
+            <br />
+            I know I want to join yours
+            <br />
+            Drop me an email or send me a message on LinkedIn 
+            </p>
+            <span>orbiccode@gmail.com</span>
+            <div>
+                <p>LinkedIn</p>
+                <p>GitHub</p>
+            </div>
         </section>
     )
 }
